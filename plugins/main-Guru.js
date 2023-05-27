0f7aca4c57443766ac8e50df50a63a41c96f8aba
Herozxc <133654973+Herozxc@users.noreply.github.com>
2023-05-26 22:47:15 -0700

    Update main-Guru.js

--- plugins/main-Guru.js
+++ plugins/main-Guru.js
@@ -3,26 +3,11 @@ let handler = async m => m.reply(`
 ≡  *GURU  ┃ ᴮᴼᵀ*   GROUPS
 
 ─────────────
-▢ Join public bot group and support
-https://chat.whatsapp.com/F3sB3pR3tClBvVmlIkqDJp
-
-▢ Group 2
-https://chat.whatsapp.com/LwLQab5mZVW6WWRbBPnDGC
-
-▢ Group 3
-https://chat.whatsapp.com/Jn9qVerYN6Q1L0vlVFe2XL
-
-─────────────
-≡ Disabled links? enter here! 
-
-▢ Group WhatsApp 
- https://chat.whatsapp.com/F3sB3pR3tClBvVmlIkqDJp
-─────────────
-▢ *Owner Telegram*
- https://t.me/i_want_to_be_isekaied
+▢ support
+wa.me/94764497078
 
 ▢ *YouTube*
-• https://www.youtube.com/@Asliguru
+• https://www.youtube.com/@SANDARU-YT
 
 
 `.trim())
